198eea31ad867249d226491b955ce5742c93db97
Charly Cimino <carlosecimino@gmail.com>
2021-06-17 17:28:49 -0300

    ConexionBD actualizada

--- src/agenda/modelo/clases/ConexionBD.java
+++ src/agenda/modelo/clases/ConexionBD.java
@@ -15,12 +15,18 @@ public class ConexionBD {
 
     private static Connection con;
 
-    public static Connection getConexion(String driver, String url, String user, String pass) throws SQLException, ClassNotFoundException {
+    public static Connection getConexion(String driver, String url, String user, String pass) {
         if (con == null) {
-            Runtime.getRuntime().addShutdownHook(new MiShutDownHook()); // Agregar hook de fin de programa
-            Class.forName(driver); // Chequeo de Driver (sujeto a excepciones)
-            con = DriverManager.getConnection(url, user, pass); // Obtener la conexión
-            System.out.println("Conexión exitosa: " + con.getClass().getName());
+            try {
+                Runtime.getRuntime().addShutdownHook(new MiShutDownHook()); // Agregar hook de fin de programa
+                Class.forName(driver); // Chequeo de Driver (sujeto a excepciones)
+                con = DriverManager.getConnection(url, user, pass); // Obtener la conexión
+                System.out.println("Conexión exitosa: " + con.getClass().getName());
+            } catch (ClassNotFoundException ex) {
+                throw new RuntimeException("No se encuentra driver " + driver, ex);
+            } catch (Exception ex) {
+                throw new RuntimeException("No se pudo establecer conexión con la BD", ex);
+            }
         }
         return con;
     }
@@ -37,15 +43,16 @@ public class ConexionBD {
     private static class MiShutDownHook extends Thread {
 
         /* Justo antes de finalizar el programa la JVM invocará
-           a este metodo donde podemos cerrar la conexion */
+           a este método donde podemos cerrar la conexion */
+        @Override
         public void run() {
             try {
                 if (con != null) {
                     con.close();
-                    System.out.println("Recursos liberados");
+                    System.out.println("Connection liberada");
                 }
             } catch (Exception ex) {
-                System.out.println("Error liberando recursos al terminar app: " + ex.getMessage());
+                System.out.println("Error liberando Connection al finalizar app: " + ex.getMessage());
             }
         }
     }
